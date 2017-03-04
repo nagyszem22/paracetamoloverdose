584d0fdf2312c20727278b88933f9c7d32745cf5
Irina Popi <irina.popi@gmai.com>
2017-03-04 19:59:54 +0000

    no message

--- app/Http/Controllers/v1/AppController.php
+++ app/Http/Controllers/v1/AppController.php
@@ -6,26 +6,27 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\v1\FormValidator;
+use Validator;
 
 class AppController extends Controller
 {
-	protected $validator;
-
-	public function __construct() 
-	{
-		$this->validator = new FormValidator(); // http://stackoverflow.com/questions/737385/easiest-form-validation-library-for-php
-	}
-
-
    	public function maindetails($date = 0, $suNumber = 0, $birthday = 0, $nhsNumber = 0, $ingestionTime = 0) 
    	{
    		// @todo: validate
-   		return $this->validator('blah@bla.', 'email');
+        // documentation: https://laravel.com/docs/5.2/validation#other-validation-approaches
+        // documentation: https://laravel.com/docs/5.2/validation#available-validation-rules
 
+        $validator = Validator::make(['title' => 'sad'], [
+            'title' => 'required|max:4',
+        ]);
 
+        if ($validator->fails()) {
+            return 'hello world';
+        }
+
+        return 'hi';
 
    		// @todo: connect to openEhr and upload the patient data
-  		// @send back a response
+  		// @todo: send back a response
    	}
 }
